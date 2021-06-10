783ac41559883b79d4490763d4927afc0eead81f
Taek Jin Han <tj.han@yale.edu>
2021-06-10 12:03:55 +0900

    commit tree fc5bf3e82958c857836e3e864ccf93e418fefe42

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+
+class Dog 
+    def name=(dog_name)
+        @this_dog_name = dog_name
+    end
+    def name
+        @this_dog_name
+    end
+end
+
+lassie = Dog.new 
+lassie.name = "Lassie"
+
+puts lassie.name 
